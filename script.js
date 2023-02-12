884f67b7ec3dcfb7ed69d729fcc69ed0a8b14b5b
IsaEscaraber <isa.escaraber@gmail.com>
2023-02-12 20:16:04 -0300

    feat: add new project about Rick and Morty

---
+++ script.js
@@ -0,0 +1,10 @@
+fetch("https://rickandmortyapi.com/api/character", {
+        method: "GET",
+      }) // I will use this fuction for API
+        .then((response) => response.json()) // Resposta Resolved
+        /*.then((json) => console.log(json)); // printing in the index */
+        .then(functin(json){
+
+        //  var container= document.queryselector('container');
+
+        })
