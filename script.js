93e0b38eb664cf2533febb0eeb99ea865a801297
IsaEscaraber <isa.escaraber@gmail.com>
2023-02-14 20:22:20 -0300

    Feat: Finished the first part, such as the design

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-fetch("https://rickandmortyapi.com/api/character", {
+fetch("https://rickandmortyapi.com/api/episode", {
         method: "GET",
       }) // I will use this fuction for API
         .then((response) => response.json()) // Resposta Resolved
         /*.then((json) => console.log(json)); // printing in the index */
         .then(functin(json){
 
-        //  var container= document.queryselector('container');
+        var containerCard = document.queryselector('containerCard');
 
         })
